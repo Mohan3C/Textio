1244743d7b108a46627ea89347b280432ba93825
Mohan <mohan845sharma45@gmail.com>
2025-09-09 10:08:22 +0530

    first big change

--- projecttextio/apptextio/views.py
+++ projecttextio/apptextio/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def home(request):
     return render(request,"main.html")
+
+
+def registeruser(request):
+  form = UserCreationForm(request.POST or None)
+
+  if request.method == "POST":
+    if form.is_valid():
+      form.save()
+      return redirect("login")  
+  else:
+    form = UserCreationForm()
+
+  return render(request,"registration/registration.html",{"form":form})
--- projecttextio/projecttextio/settings.py
+++ projecttextio/projecttextio/settings.py
@@ -38,7 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "apptextio",
+    'crispy_forms',
+    'crispy_tailwind',
+
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
